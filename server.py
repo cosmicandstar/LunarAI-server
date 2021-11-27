6de2f746f77f4c22c2a8bf0d016568a81b7853a4
dungbik <q1w2e30630@gmail.com>
2021-11-27 21:10:18 +0900

    Feat: 예전 코드로 되돌림

--- server.py
+++ server.py
@@ -1,21 +1,22 @@
 import base64
 import cv2
 import zmq
-import time
 
 context = zmq.Context()
 
-camera = cv2.VideoCapture('tcp://192.168.0.26:3003')  # 카메라와 연결
-
 send_socket = context.socket(zmq.PUB)
 send_socket.bind('tcp://*:5000')
-last_time = time.perf_counter()
 
-while camera.isOpened():
-    grabbed, frame = camera.read()
-    if (time.perf_counter() - last_time) > (1 / 10.0):
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        encoded, buffer = cv2.imencode('.jpg', gray_frame)
-        jpg_as_text = base64.b64encode(buffer)
-        send_socket.send(jpg_as_text)
-        last_time = time.perf_counter()
+recv_socket = context.socket(zmq.SUB)
+recv_socket.connect('tcp://localhost:4000')  # 라즈베리파이 ip
+recv_socket.setsockopt_string(zmq.SUBSCRIBE, np.compat.unicode(''))
+
+while True:
+    frame = recv_socket.recv_string()
+    img = base64.b64decode(frame)
+    np_img = np.frombuffer(img, dtype=np.uint8)
+    source = cv2.imdecode(np_img, 1)
+    gray_frame = cv2.cvtColor(source, cv2.COLOR_BGR2GRAY)
+    encoded, buffer = cv2.imencode('.jpg', gray_frame)
+    jpg_as_text = base64.b64encode(buffer)
+    send_socket.send(jpg_as_text)
