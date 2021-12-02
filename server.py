8d12f73768ef8044f75df2fce5389158cdef527b
dungbik <q1w2e30630@gmail.com>
2021-12-02 23:21:57 +0900

    Feat: 얼굴 학습 요청 처리되게 수정

--- server.py
+++ server.py
@@ -1,6 +1,39 @@
 import base64
 import cv2
 import zmq
+import numpy as np
+import threading
+
+
+def face_model_fit(id):
+    # todo 얼굴 학습되게 코드 넣기
+    print("fit :", id)
+
+
+def process(recv_socket):
+    while True:
+        frame = recv_socket.recv_string()
+        data = frame.split(':')
+        header = data[0]
+        if header == 'fit':
+            id = data[1]
+            face_model_fit(id)
+
+
+def face_model(recv_socket, send_socket):
+    while True:
+        frame = recv_socket.recv_string()
+        img = base64.b64decode(frame)
+        np_img = np.frombuffer(img, dtype=np.uint8)
+        source = cv2.imdecode(np_img, 1)
+
+        # todo 모델 코드 작성 (현재는 흑백으로 변환되게 해둠)
+        gray_frame = cv2.cvtColor(source, cv2.COLOR_BGR2GRAY)
+
+        encoded, buffer = cv2.imencode('.jpg', gray_frame)
+        jpg_as_text = base64.b64encode(buffer)
+        send_socket.send(jpg_as_text)
+
 
 context = zmq.Context()
 
@@ -8,15 +41,15 @@ send_socket = context.socket(zmq.PUB)
 send_socket.bind('tcp://*:5000')
 
 recv_socket = context.socket(zmq.SUB)
-recv_socket.connect('tcp://localhost:4000')  # 라즈베리파이 ip
+recv_socket.connect('tcp://192.168.0.14:4000')  # 라즈베리파이 ip
 recv_socket.setsockopt_string(zmq.SUBSCRIBE, np.compat.unicode(''))
 
-while True:
-    frame = recv_socket.recv_string()
-    img = base64.b64decode(frame)
-    np_img = np.frombuffer(img, dtype=np.uint8)
-    source = cv2.imdecode(np_img, 1)
-    gray_frame = cv2.cvtColor(source, cv2.COLOR_BGR2GRAY)
-    encoded, buffer = cv2.imencode('.jpg', gray_frame)
-    jpg_as_text = base64.b64encode(buffer)
-    send_socket.send(jpg_as_text)
+recv_socket2 = context.socket(zmq.SUB)
+recv_socket2.connect('tcp://localhost:6000')
+recv_socket2.setsockopt_string(zmq.SUBSCRIBE, np.compat.unicode(''))
+
+face_model_thread = threading.Thread(target=face_model, args=(recv_socket, send_socket))
+process_thread = threading.Thread(target=process, args=(recv_socket2, ))
+
+face_model_thread.start()
+process_thread.start()
